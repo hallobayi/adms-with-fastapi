f568cfe0581ccb79660ddc868b7bec0fe9102580
mDestaFadilah <desta.08b@gmail.com>
2024-07-23 17:31:54 +0700

    exception

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = { "foo" : "The Foo Wrestless" }
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item": items[item_id]}
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
